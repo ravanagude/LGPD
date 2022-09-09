81a3967a03ea7bc5ad839aec2be88bbfac16f075
Ravana <79427123+ravanagude@users.noreply.github.com>
2022-09-10 01:22:16 +0200

    conficuracion del setup

--- app/src/main/java/br/com/joshuaweb/lgpd/AuthActivity.kt
+++ app/src/main/java/br/com/joshuaweb/lgpd/AuthActivity.kt
@@ -1,8 +1,12 @@
 package br.com.joshuaweb.lgpd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +24,63 @@ class AuthActivity : AppCompatActivity() {
         bundle.putString("message", "Integración de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
+        //Setup
+        setup()
     }
+
+    private fun setup() {
+
+        title = "Autenticación"
+
+        singUpButton.setOnClickListener {it:View!
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(),passwordEditText.text.toString()).addOnCanceledListener {it: Task<AuthResult>
+
+                if (it.isSuccessful){
+                    showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                }else{
+                    showAlert()
+                }
+
+                }
+            }
+        }
+
+        logInButton.setOnClickListener {it:View!
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().singInWithEmailAndPassword(emailEditText.text.toString(),passwordEditText.text.toString()).addOnCanceledListener {it: Task<AuthResult>
+
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+
+                }
+            }
+        }
+
+
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(context: this)
+        builder.setTitle("Error")
+        builder.setMessage("Erro de autenticação de usuário")
+        builder.setPositiveButton(text:"Aceptar", listener:null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
+        val homeIntent:Intent = Intent(packageContext:this, HomeActivity::class.java).apply {
+            putExtra(name:"email", email)
+            putExtra(name:"provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+
 }
--- app/src/main/java/br/com/joshuaweb/lgpd/HomeActivity.kt
+++ app/src/main/java/br/com/joshuaweb/lgpd/HomeActivity.kt
@@ -2,10 +2,34 @@ package br.com.joshuaweb.lgpd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC
+}
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Setup
+
+        val bundle:Bundle? = intent.extras
+        val email:String? = bundle?.getString(key:"email")
+        val provider: String? = bundle?.getString(key:"provider")
+        setup(email:email ?: "", provider: provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+        title = "Inicio"
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        LogOutButton.setOnClickListener{it:View!
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
     }
 }
